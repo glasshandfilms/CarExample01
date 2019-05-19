218abcbcb64bf186c298f5d0c06cd011a345ac8b
glasshandfilms <brandon@glasshandstudios.com>
2019-05-19 07:10:31 -0400

    upgrade to 2019.3alpha HDRP

---
+++ Assets/Scripts/focusPull.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
+
+public class focusPull : MonoBehaviour
+{
+    
+    DepthOfField dofComponent;
+    public MinFloatParameter focDist;
+    public float focDistAnim;
+    
+     
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Volume volume = gameObject.GetComponent<Volume>();
+        DepthOfField tmp;
+        if(volume.profile.TryGet<DepthOfField>(out tmp))
+        {
+            dofComponent = tmp;
+        }           
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        focDist = dofComponent.focusDistance;
+        focDist.value = focDistAnim;
+    }
+}
