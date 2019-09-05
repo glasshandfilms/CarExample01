90717187c34168e2cd495556a5b495c256b438e6
glasshandstudios <brandon@glasshandstudios.com>
2019-09-05 18:01:11 -0400

    updated to unity 2019.3b

--- Assets/Scripts/focusPull.cs
+++ Assets/Scripts/focusPull.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.HDPipeline;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class focusPull : MonoBehaviour
 {
